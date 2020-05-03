a6fcf2e6980f54bd171cd7281ff5a1cacdc16f35
yassminat <yasminekamel1@gmail.com>
2020-05-03 19:20:51 -0400

    displaying updated css background gradient values

--- backgroundGenerator/script.js
+++ backgroundGenerator/script.js
@@ -1,15 +1,15 @@
 const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
-const body = document.querySelector("body");
+const body = document.getElementById("gradient");
 
 function adjustGradient() {
-    body.style.background =
-        "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-}
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
+  css.textContent = body.style.background + ";";
+}
 
 color1.addEventListener("input", adjustGradient);
 
 color2.addEventListener("input", adjustGradient);
-
